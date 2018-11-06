b2dc119e2fe276534005de6ce4f3ee7ccb647221
Janmax <tomix.max123@o2.pl>
2018-11-06 16:54:05 +0000

    Use SSL and the Puma webserver

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,10 +5,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", 'home'
-    assert_select "a[href=?]", '/help'
-    assert_select "a[href=?]", '/about'
-    assert_select "a[href=?]", '/contact'
+    assert_select "a[href=?]", root_path
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
     
     get contact_path
     assert_select "title", full_title("Contact")
