d36c945d1921ff783d05365c98862f4586cb375f
Janmax <tomix.max123@o2.pl>
2018-11-09 20:09:10 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Fantastycznie ze jestes! Witam w The Sample App!"
-      # Lubie Placki
       redirect_to @user
     else
       render 'new'
