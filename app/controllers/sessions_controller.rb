f042db932d4b1d0f17b1f6ebe39f0e04a63b3981
Janmax <tomix.max123@o2.pl>
2018-11-18 23:36:53 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
+    #debugger
   end
 
   def create
@@ -8,9 +9,8 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+      redirect_back_or user
     else
-      #flash[:danger] = 'Invalid email/password combination' # Red TEST
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
